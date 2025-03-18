f165a24868dcbf189f61b732c6f12f55233b1495
vicenzobaricelli <baricelliv@gmail.com>
2025-03-18 13:11:24 -0400

    Force update: Replacing all files with new project

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const path = require('path');
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
---
+++ src/app/ui/dashboard/nav-links.jsx
@@ -0,0 +1,38 @@
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/ui/dashboard', icon: HomeIcon },
+  {
+    name: 'Invoices',
+    href: '/ui/invoices',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Customers', href: '/ui/customers', icon: UserGroupIcon },
+];
+
+export default function NavLinks() {
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
